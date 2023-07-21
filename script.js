d7a12a51ae3d449235df01f17d2e85c03ecb1d28
Nwabueze Okemini <122814541+NwabuezeM@users.noreply.github.com>
2023-07-21 13:58:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector('.fa-bars');
+const sidebar = document.querySelector('.nav-main');
+const container = document.querySelector('.container');
+
+hamburger.addEventListener('click', () => {
+    sidebar.classList.toggle('show-sidebar');
+    if (!sidebar.classList.contains('show-sidebar')) {
+        hamburger.style.marginLeft = '0';
+        container.style.marginLeft = '0';
+    } else {
+        hamburger.style.marginLeft = '220px';
+        container.style.marginLeft = '220px';
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if (!sidebar.contains(event.target) && hamburger !== event.target) {
+        sidebar.classList.remove('show-sidebar');
+        hamburger.style.marginLeft = '0';
+        container.style.marginLeft = '0';
+    }
+});
