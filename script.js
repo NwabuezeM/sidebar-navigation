061f150402c0b94d004fd48edeaa6471d8974d9d
Nwabueze Okemini <122814541+NwabuezeM@users.noreply.github.com>
2023-07-21 23:18:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,7 @@ const container = document.querySelector('.container');
 const removeSidebar = document.querySelector('.fa-times');
 
 const toggleSidebar = () => {
-    sidebar.classList.toggle('show-sidebar');
-    if (!sidebar.classList.contains('show-sidebar')) {
-        removeMargin();
-    } else {
-        hamburger.style.marginLeft = '180px';
-        container.style.marginLeft = '185px';
-    }
+    sidebar.classList.add('show-sidebar');
 };
 
 hamburger.addEventListener('click', () => {
@@ -19,7 +13,6 @@ hamburger.addEventListener('click', () => {
 
 removeSidebar.addEventListener('click', () => {
     sidebar.classList.remove('show-sidebar');
-    removeMargin();
 });
 
 document.addEventListener('click', (event) => {
@@ -33,14 +26,8 @@ const mediaQuery = window.matchMedia('(min-width: 769px)');
 const handleMediaQueryChange = (event) => {
     if (event.matches) {
         sidebar.classList.remove('show-sidebar');
-        removeMargin();
     }
 };
-
-const removeMargin = () => {
-    hamburger.style.marginLeft = '0';
-    container.style.marginLeft = '0';
-}
 
 mediaQuery.addEventListener('change', handleMediaQueryChange);
 
